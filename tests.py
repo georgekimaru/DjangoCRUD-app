89b3137d2adfe6263af8be7ad90cc6fe730dcb8b
George Kimaru <george.kimaru98@gmail.com>
2021-11-10 12:41:51 +0300

    Updated Chrome driver

--- tests.py
+++ tests.py
@@ -6,5 +6,5 @@ class HostTest(LiveServerTestCase):
     def testhomepage(self):
         driver = webdriver.Chrome()
 
-        driver.get('http://127.0.0.1:8000/')
+        driver.get('http://127.0.0.1:8000/emp')
         assert "Hello, world" in driver.title
